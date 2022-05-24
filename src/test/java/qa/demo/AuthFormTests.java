5350b01ad0fe92fc496d12849b0f28dca9b7f9c5
tsolod <40228890+tsolod@users.noreply.github.com>
2022-05-24 21:42:48 +0300

    added full test

--- src/test/java/qa/demo/AuthFormTests.java
+++ src/test/java/qa/demo/AuthFormTests.java
@@ -5,8 +5,9 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AuthFormTests {
@@ -41,7 +42,14 @@ public class AuthFormTests {
         $(".react-datepicker__day--021").click();
         $("#subjectsInput").setValue(subjects).pressEnter();
         $$(".custom-checkbox").get(1).click();
-        $("input#uploadPicture").uploadFromClasspath("cat.PNG");
+        $("input#uploadPicture").uploadFile(new File("src\\test\\java\\resources\\cat.png"));
+        $("#currentAddress").setValue(currentAddress);
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Noida").pressEnter();
+        $(".btn-primary").click();
+        $(".modal-body").shouldHave(text("Anna Banana"),text(email),text("Male"),text("21 November,1995"),
+                text(subjects),text("Reading"),text("cat.PNG"),text(currentAddress),text("NCR Noida"));
+
 
 
 
